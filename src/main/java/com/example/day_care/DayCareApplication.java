3b9039c61198b1c84d61608a6d30c38372e0828b
Paweł Stępień <pstepien@interia.pl>
2021-03-16 14:05:04 +0100

    added hello pawel to the main application

--- src/main/java/com/example/day_care/DayCareApplication.java
+++ src/main/java/com/example/day_care/DayCareApplication.java
@@ -1,3 +1,4 @@
+// hello pawel
 package com.example.day_care;
 
 import org.springframework.boot.SpringApplication;
@@ -5,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DayCareApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DayCareApplication.class, args);
 	}
