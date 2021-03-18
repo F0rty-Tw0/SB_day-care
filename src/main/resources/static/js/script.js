db75710357b2a416bdfe3a9c43c7add3803b5a48
Artiom Tofan <artiomtofancv@gmail.com>
2021-03-18 16:07:53 +0100

    made cool animation on navbar

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,19 @@
+const onReady = (callback) => {
+	var intervalId = window.setInterval(function() {
+		if (document.getElementsByTagName('body')[0] !== undefined) {
+			window.clearInterval(intervalId);
+			callback.call(this);
+		}
+	}, 350);
+};
+
+const setVisible = (selector, visible) => {
+	document.querySelector(selector).style.display =
+		visible ? 'block' :
+		'none';
+};
+
+onReady(() => {
+	setVisible('.page', true);
+	setVisible('#loading', false);
+});
